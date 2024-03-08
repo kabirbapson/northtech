3756316873a29adbf376b965179e88095f171367
Bappi Kabir <kabirbapson@gmail.com>
2024-03-08 18:44:02 +0100

    hhome

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -13,13 +13,13 @@ const Intro = () => {
                         </div>
                         <div className="flex-col my-4 text-center lg:text-left lg:my-0 lg:justify-end w-full lg:w-1/2 px-8" data-aos="zoom-in" data-aos-delay="500">
                             
-                            <h3 className="text-3xl  text-blue-900 font-bold">We develop high quality bespoke web and mobile applications for organizations, institutions and SMEs</h3>
+                            <h3 className="text-3xl  text-blue-900 font-bold">Objective</h3>
                             <div>
-                                <p className='my-3 text-xl text-gray-600 font-semibold'>Our team is well vast in software development and is ready to help develop the applications of your choice.</p>
+                                <p className='my-3 text-xl text-gray-600 font-semibold'>At NORTHteck.io, we aim to transform the tech industry by offering innovative solutions that empower businesses to excel in the digital era. We are dedicated to supporting startups and established companies with a wide range of consulting services.</p>
                             </div>
                             
                             <div>
-                                <p className='my-3 text-xl text-gray-600 font-semibold'>We take responsibility for building custom software solutions that caters for automation of your business processes and improve efficiency.</p>
+                                <p className='my-3 text-xl text-gray-600 font-semibold'>Our main focus is to help startups secure funding, conduct research, and implement effective marketing strategies for growth and success.</p>
                             </div>
                             <Link to="/contact" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-2 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0 group">
                                 Contact us
